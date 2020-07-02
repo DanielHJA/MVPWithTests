29678b832656aaf9bd82b51e542ef7fdf59595b9
Daniel Hjartstrom <daniel.hjartstrom@consid.se>
2020-07-02 10:38:43 +0200

    Initial Commit

---
+++ MPVUnitTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MPVUnitTesting
+//
+//  Created by Daniel Hjärtström on 2020-07-02.
+//  Copyright © 2020 Daniel Hjärtström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MPVUnitTestingTests/MPVUnitTestingTests.swift
@@ -0,0 +1,34 @@
+//
+//  MPVUnitTestingTests.swift
+//  MPVUnitTestingTests
+//
+//  Created by Daniel Hjärtström on 2020-07-02.
+//  Copyright © 2020 Daniel Hjärtström. All rights reserved.
+//
+
+import XCTest
+@testable import MPVUnitTesting
+
+class MPVUnitTestingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
